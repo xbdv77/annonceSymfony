641c5b618813eb1740d8ca010f5bc52d9139c849
Xavier Haltebourg <xhaltebourg@umanis.com>
2017-03-03 15:57:51 +0100

    integration du FOS/UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new XHG\PlateformBundle\XHGPlateformBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
